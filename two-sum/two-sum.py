4c890391ed6fefeea4cb1cb4ab0cbfebb4f2b12e
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-07 01:11:41 +0300

    update two sum

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -5,8 +5,6 @@ def two_sum(nums: List[int], target: int) -> List[int]:
         if len(nums) == 2:
             return [0, 1]
         
-        d = {}
-        
         for i in range(0, len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
