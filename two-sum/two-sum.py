2cdf598187e55dc85355a69f343d4cb67690080f
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-07 01:12:34 +0300

    update two sum

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -2,10 +2,10 @@ from typing import List
 
 
 def two_sum(nums: List[int], target: int) -> List[int]:
-        if len(nums) == 2:
-            return [0, 1]
-        
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+    if len(nums) == 2:
+        return [0, 1]
+    
+    for i in range(0, len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
