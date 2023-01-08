4a6bbca4ce9762ffab80aaaaa4187a55fcb9f6bb
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-08 11:26:16 +0300

    longest common prefix

--- longest-common-prefix/longest_common_prefix.py
+++ longest-common-prefix/longest_common_prefix.py
@@ -1,1 +1,15 @@
+from typing import List
 
+
+def longestCommonPrefix(strs: List[str]) -> str:
+    res = ""
+    
+    for i in range(len(strs[0])):
+        for j in range(len(strs)):
+            if len(strs[j]) - 1 >= i:
+                if strs[0][i] != strs[j][i]:  
+                    return res
+                continue
+            return res
+        res += strs[0][i]
+    return res
