0e49e26a2507933948a7e223b6af187a6270a601
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-05 01:12:35 +0300

    longest palindrome v2

--- longest-palindrome/longest_palindrome.py
+++ longest-palindrome/longest_palindrome.py
@@ -18,3 +18,40 @@ def longestPalindrome(s: str) -> int:
             res += value - 1
     res += max_odd
     return res
+
+
+def longestPalindrome(s: str) -> int:
+    """
+    For each letter, say it occurs v times. We know we have v // 2 * 2 letters
+    that can be partnered for sure. For example, if we have 'aaaaa', then we
+    could have 'aaaa' partnered, which is 5 // 2 * 2 = 4 letters partnered.
+
+    At the end, if there was any v % 2 == 1, then that letter could have been
+    a unique center. Otherwise, every letter was partnered. To perform this
+    check, we will check for v % 2 == 1 and ans % 2 == 0, the latter meaning
+    we haven't yet added a unique center to the answer.
+
+    In other words:
+        If we have an odd number of letters partnered, then we can add one
+        unique center to our answer. Otherwise, we cannot.
+        Before loop ends, we will add all odd numbers to the pairs and
+        if there is still a unique center, existing odd numbers, we will
+        add 1 to the answer. But we can add unique center only once, that's
+        why we check if unpaired_chars exists, if it does, we add 1 to the
+        answer, otherwise we don't.
+
+    Time: O(n)
+    Space: O(1)
+    """
+
+    pairs = 0
+    unpaired_chars = set()
+    
+    for char in s:
+        if char in unpaired_chars:
+            pairs += 1
+            unpaired_chars.remove(char)
+        else:
+            unpaired_chars.add(char)
+    
+    return pairs * 2 + 1 if unpaired_chars else pairs * 2
