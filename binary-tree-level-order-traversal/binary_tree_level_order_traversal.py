c2601c9f65d05e91a23ee37c44bdee176d5f90fa
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-06 01:54:40 +0300

    binary tree level order traversal v2 iterative

--- binary-tree-level-order-traversal/binary_tree_level_order_traversal.py
+++ binary-tree-level-order-traversal/binary_tree_level_order_traversal.py
@@ -34,3 +34,24 @@ def levelOrderV1Recursive(root: Optional[TreeNode]) -> List[List[int]]:
 
     helper(root, 0)
     return result
+
+
+def levelOrderV2Iterative(root: Optional[TreeNode]) -> List[List[int]]:
+    if not root:
+        return []
+
+    queue = [root]
+    result = []
+    while queue:
+        level = []
+
+        for _ in range(len(queue)):
+            node = queue.pop(0)
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        result.append(level)
+    return result
