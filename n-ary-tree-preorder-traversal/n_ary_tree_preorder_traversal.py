de8788a3da9390faf04cbfda30350031494f9fd6
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-06 01:17:25 +0300

    n ary tree preorder traversal v2 iterative

--- n-ary-tree-preorder-traversal/n_ary_tree_preorder_traversal.py
+++ n-ary-tree-preorder-traversal/n_ary_tree_preorder_traversal.py
@@ -15,7 +15,7 @@ class Node:
         self.children = children
 
 
-def preorderV1(root: Node) -> List[int]:
+def preorderV1Recursive(root: Node) -> List[int]:
     res = []
         
     def get_preorder(root: Node, res: List[int]) -> None:
@@ -27,3 +27,27 @@ def preorderV1(root: Node) -> List[int]:
     
     get_preorder(root, res)
     return res
+
+
+def preorderV2Iterative(root: Node) -> List[int]:
+        if root is None:
+            return []
+
+        stack = [root]
+        output = []
+
+        # Till there is element in stack the loop runs
+        while stack:
+            #pop the last element from the stack and store it into temp
+            temp = stack.pop()
+
+            # append the value of temp to output
+            output.append(temp.val)
+
+            # add the children of the temp into the stack in reverse order
+            # children of 1 = [3,2,4], if not reveresed then 4 will be popped out first from the stack.
+            # if reversed then stack = [4,2,3]. Here 3 will pop out first
+            # This continues till the stack is empty
+            if temp.children:
+                stack.extend(temp.children[::-1])
+        return output
