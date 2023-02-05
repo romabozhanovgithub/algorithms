cc357bcade411528c5bcbce2036eaf631dad72d8
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-06 02:01:07 +0300

    add comments for recursive approach

--- binary-tree-level-order-traversal/binary_tree_level_order_traversal.py
+++ binary-tree-level-order-traversal/binary_tree_level_order_traversal.py
@@ -17,21 +17,32 @@ class TreeNode:
 
 def levelOrderV1Recursive(root: Optional[TreeNode]) -> List[List[int]]:
     if not root:
+        # return an empty list if the root is None
         return []
 
-    result = []
+    # result list to store the values at each level
+    result: List[List[int]] = []
 
-    def helper(node, level):
+    def helper(node: TreeNode, level: int):
+        """
+        Helper function to traverse the tree in a pre-order fashion
+        """
+
+        # if we haven't encountered this level before, add a new list for it
         if len(result) == level:
             result.append([])
 
+        # add the current node's value to the current level list
         result[level].append(node.val)
 
         if node.left:
+            # if there is a left child, traverse it recursively, increasing the level by 1
             helper(node.left, level + 1)
         if node.right:
+            # if there is a right child, traverse it recursively, increasing the level by 1
             helper(node.right, level + 1)
 
+    # call the helper function with the root node and starting level 0
     helper(root, 0)
     return result
 
