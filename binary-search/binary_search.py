b6c098ccf6dc0c62c6b6adefbeaea71ef0542e04
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-07 21:29:15 +0300

    binary search v2 iterative

---
+++ binary-search/binary_search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def searchV2(nums: List[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if nums[mid] < target:
+            low = mid + 1
+        elif nums[mid] > target:
+            high = mid - 1
+        else:
+            return mid
+
+    return -1
