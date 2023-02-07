fabbba7d27253e4932013f97401620ef9a4844b5
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-07 21:32:34 +0300

    binary search v1 recursive

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,7 +1,24 @@
 from typing import List
 
 
-def searchV2(nums: List[int], target: int) -> int:
+def searchV1Recursive(nums: List[int], target: int) -> int:
+    def binarySearch(low: int, high: int) -> int:
+        if low > high:
+            return -1
+
+        mid = (low + high) // 2
+
+        if nums[mid] < target:
+            return binarySearch(mid + 1, high)
+        elif nums[mid] > target:
+            return binarySearch(low, mid - 1)
+        else:
+            return mid
+
+    return binarySearch(0, len(nums) - 1)
+
+
+def searchV2Iterative(nums: List[int], target: int) -> int:
     low = 0
     high = len(nums) - 1
     mid = 0
