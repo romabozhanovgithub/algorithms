bc40c151888977470501c7e4e976f5097d8eddba
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-08 23:18:27 +0300

    validate binary search tree v2 iterative

--- validate-binary-search-tree/validate_binary_search_tree.py
+++ validate-binary-search-tree/validate_binary_search_tree.py
@@ -16,3 +16,17 @@ def isValidBSTV1Recursive(root: Optional[TreeNode]) -> bool:
         else:
             return dfs(node.left, node.val, lower) and dfs(node.right, upper, node.val)
     return dfs(root, float('inf'), float('-inf'))
+
+
+def isValidBSTV2Iterative(root: Optional[TreeNode]) -> bool:
+    stack = [(root, float('inf'), float('-inf'))]
+    while stack:
+        node, upper, lower = stack.pop()
+        if not node:
+            continue
+        elif node.val >= upper or node.val <= lower:
+            return False
+        else:
+            stack.append((node.left, node.val, lower))
+            stack.append((node.right, upper, node.val))
+    return True
