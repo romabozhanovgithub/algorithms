0bf2e0bbb54b584d3233ab409ecf4c46e4671d41
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-09 22:30:41 +0300

    flood fill v1 recursive

--- flood-fill/flood_fill.py
+++ flood-fill/flood_fill.py
@@ -13,3 +13,31 @@ aforementioned pixels with color.
 
 Return the modified image after performing the flood fill.
 """
+
+
+from typing import List
+
+
+def floodFillV1Recursive(
+        image: List[List[int]], sr: int, sc: int, newColor: int
+) -> List[List[int]]:
+    R, C = len(image), len(image[0])
+    color = image[sr][sc]
+    
+    if color == newColor:
+        return image
+
+    def dfs(r, c):
+        if image[r][c] == color:
+            image[r][c] = newColor
+            if r >= 1:
+                dfs(r - 1, c)
+            if r + 1 < R:
+                dfs(r + 1, c)
+            if c >= 1:
+                dfs(r, c - 1)
+            if c + 1 < C:
+                dfs(r, c + 1)
+
+    dfs(sr, sc)
+    return image
