e9ceab002dd7b1c7ade0fbf0f5e6c3ae337c1863
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-10 19:12:44 +0300

    fibonacci number v2 iterative

--- fibonacci-number/fibonacci_number.py
+++ fibonacci-number/fibonacci_number.py
@@ -2,3 +2,13 @@ def fibV1Recursive(n: int) -> int:
     if n <= 1:
         return n
     return fibV1Recursive(n - 1) + fibV1Recursive(n - 2)
+
+
+def fibV2Iterative(n: int) -> int:
+    if n <= 1:
+        return n
+
+    a, b = 0, 1
+    for _ in range(1, n):
+        a, b = b, a + b
+    return b
