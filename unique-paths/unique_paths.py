fd75ddfd11cb4a44d4b0ea57aeb69fea9cd1a697
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-11 19:17:59 +0300

    unique paths

--- unique-paths/unique_paths.py
+++ unique-paths/unique_paths.py
@@ -7,3 +7,16 @@ move either down or right at any point in time.
 Given the two integers m and n, return the number of possible unique paths
 that the robot can take to reach the bottom-right corner.
 """
+
+
+def uniquePaths(m: int, n: int) -> int:
+    """
+    Time Complexity: O(m * n)
+    Space Complexity: O(m * n)
+    """
+    
+    dp = [[1] * n for _ in range(m)]
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[-1][-1]
