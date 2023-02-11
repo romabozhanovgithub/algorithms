c917b21999e08f5d59d421af05d24d949ac06ed2
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-11 18:43:40 +0300

    min cost climbing stairs

--- min-cost-climbing-stairs/min_cost_climbing_stairs.py
+++ min-cost-climbing-stairs/min_cost_climbing_stairs.py
@@ -6,3 +6,16 @@ You can either start from the step with index 0, or the step with index 1.
 
 Return the minimum cost to reach the top of the floor.
 """
+
+
+from typing import List
+
+
+def minCostClimbingStairs(cost: List[int]) -> int:
+    """Return the minimum cost to reach the top of the floor."""
+
+    if len(cost) == 2:
+        return min(cost)
+    for i in range(2, len(cost)):
+        cost[i] += min(cost[i - 1], cost[i - 2])
+    return min(cost[-1], cost[-2])
