f3f4a5fe0f5c38fecd89138466b82f4878e96124
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-13 20:06:58 +0300

    bulls and cows

--- bulls-and-cows/bulls_and_cows.py
+++ bulls-and-cows/bulls_and_cows.py
@@ -17,3 +17,31 @@ for your friend's guess.
 The hint should be formatted as "xAyB", where x is the number of bulls and y is
 the number of cows. Note that both secret and guess may contain duplicate digits.
 """
+
+
+def getHint(secret: str, guess: str) -> str:
+    """
+    >>> getHint("1807", "7810")
+    '1A3B'
+    >>> getHint("1123", "0111")
+    '1A1B'
+    >>> getHint("1", "0")
+    '0A0B'
+    >>> getHint("1", "1")
+    '1A0B'
+    """
+    
+    bulls = 0
+    cows = 0
+    secret_dict = {}
+    guess_dict = {}
+    for i in range(len(secret)):
+        if secret[i] == guess[i]:
+            bulls += 1
+        else:
+            secret_dict[secret[i]] = secret_dict.get(secret[i], 0) + 1
+            guess_dict[guess[i]] = guess_dict.get(guess[i], 0) + 1
+    for key in guess_dict:
+        if key in secret_dict:
+            cows += min(guess_dict[key], secret_dict[key])
+    return f"{bulls}A{cows}B"
