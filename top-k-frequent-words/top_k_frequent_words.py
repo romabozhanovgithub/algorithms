a551645690c34a1dfc1fcec1483f7b85f17df839
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-13 20:00:48 +0300

    top k frequent words

--- top-k-frequent-words/top_k_frequent_words.py
+++ top-k-frequent-words/top_k_frequent_words.py
@@ -5,3 +5,18 @@ return the k most frequent strings.
 Return the answer sorted by the frequency from highest to lowest.
 Sort the words with the same frequency by their lexicographical order.
 """
+
+
+from typing import List
+
+
+def topKFrequent(words: List[str], k: int) -> List[str]:
+    d = {}
+    for word in words:
+        if word not in d:
+            d[word] = 0
+        d[word] += 1
+
+    l = list(d.items())
+    l.sort(key=lambda x: (-x[1], x[0]))
+    return [i[0] for i in l[:k]]
