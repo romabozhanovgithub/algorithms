fc8594340f7e2663c42529ed3ed226d2a03151bc
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-14 13:01:23 +0300

    last stone weight heap

--- last-stone-weight/last_stone_weight.py
+++ last-stone-weight/last_stone_weight.py
@@ -45,3 +45,20 @@ def lastStoneWeightSortAndInsertV2(stones: List[int]) -> int:
             stones.append(y - x)
             stones.sort()
     return 0
+
+
+def lastStoneWeightHeap(stones: List[int]) -> int:
+    """
+    Use a heap to keep track of the heaviest stone
+    """
+    import heapq
+
+    stones = [-stone for stone in stones]
+    heapq.heapify(stones)
+
+    while len(stones) > 1:
+        y = heapq.heappop(stones)
+        x = heapq.heappop(stones)
+        if x != y:
+            heapq.heappush(stones, y - x)
+    return -stones[0] if stones else 0
