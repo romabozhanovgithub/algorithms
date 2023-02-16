2fba33eaab1ecb1ec9bea133c88cff155313189b
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-16 22:14:42 +0300

    maximum depth of binary tree v1 recursive

--- maximum-depth-of-binary-tree/maximum_depth_of_binary_tree.py
+++ maximum-depth-of-binary-tree/maximum_depth_of_binary_tree.py
@@ -4,3 +4,19 @@ Given the root of a binary tree, return its maximum depth.
 A binary tree's maximum depth is the number of nodes along
 the longest path from the root node down to the farthest leaf node.
 """
+
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def maxDepthV1Recursive(root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+    return 1 + max(maxDepthV1Recursive(root.left), maxDepthV1Recursive(root.right))
