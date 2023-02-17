3de00b2f50bd1db79bceda76ff6d165d9473dbee
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-17 19:59:19 +0300

    add minimum difference between bst nodes v1 recursive

--- minimum-distance-between-bst-nodes/minimum_distance_between_bst_nodes.py
+++ minimum-distance-between-bst-nodes/minimum_distance_between_bst_nodes.py
@@ -2,3 +2,27 @@
 Given the root of a Binary Search Tree (BST), return the minimum
 difference between the values of any two different nodes in the tree.
 """
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def minDiffInBSTV1Recursive(root: Optional[TreeNode]) -> int:
+    res = [float("inf"), -1] # [min, prev]
+
+    def minDiffInBST(root: Optional[TreeNode], res) -> int:
+        if root.left:
+            minDiffInBST(root.left, res)
+        if res[1] != -1:
+            res[0] = min(res[0], root.val - res[1])
+        res[1] = root.val
+        if root.right:
+            minDiffInBST(root.right, res)
+        return res[0]
+    return minDiffInBST(root, res)
