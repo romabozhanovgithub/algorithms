3434ae06aa7534ef734a0de716cdb2217e97a978
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-20 22:22:17 +0300

    single element in a sorted array v1

--- single-element-in-a-sorted-array/single_element_in_a_sorted_array.py
+++ single-element-in-a-sorted-array/single_element_in_a_sorted_array.py
@@ -7,3 +7,21 @@ Return the single element that appears only once.
 
 Your solution must run in O(log n) time and O(1) space.
 """
+
+from typing import List
+
+
+def singleNonDuplicateV1(nums: List[int]) -> int:
+    if len(nums) == 1:
+        return nums[0]
+
+    low = 0
+    high = -1
+    while low <= high:
+        if nums[low] != nums[low + 1]:
+            return nums[low]
+        elif nums[high] != nums[high -  1]:
+            return nums[high]
+        low += 2
+        high -= 2
+    return -1
