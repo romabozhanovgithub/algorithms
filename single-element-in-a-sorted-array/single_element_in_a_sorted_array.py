8612580f894bcdce18189ed7d5b15a844905eedc
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-02-20 22:31:32 +0300

    single element in a sorted array v2

--- single-element-in-a-sorted-array/single_element_in_a_sorted_array.py
+++ single-element-in-a-sorted-array/single_element_in_a_sorted_array.py
@@ -25,3 +25,30 @@ def singleNonDuplicateV1(nums: List[int]) -> int:
         low += 2
         high -= 2
     return -1
+
+
+def singleNonDuplicateV2(nums: List[int]) -> int:
+    # Initialize left and right bounds for binary search
+    l, r = 0, len(nums) // 2 
+    ans = -1
+    
+    # Binary search loop
+    while l <= r: 
+        # Calculate the middle between left and right bound
+        mid = (l + r) // 2
+        # Multiply the middle by 2 as each pair is represented by 2 elements in the array
+        idx = mid * 2
+        
+        # If the selection is outside of the number of elements in the array or if the current element does not equal it's pair
+        if idx + 1 >= len(nums) or nums[idx] != nums[idx + 1]:
+            # Decrease the right bound
+            r = mid - 1
+            # Set the ans to the value at the index
+            ans = nums[idx]
+        # The current element equals it's pair
+        else:
+            # Increase the left bound
+            l = mid + 1
+    # Return ans
+    return ans
+    
